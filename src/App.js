6155e4e429b3ea3b7f14ff9388107fe789638024
Mohamed Elselhab <momanchester1992@gmail.com>
2023-11-06 12:35:43 +0000

    create shopping cart

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { useGlobalContext } from "./context";
+import Navbar from "./Navbar";
+import CartContainer from "./CartContainer";
+// items
+
+function App() {
+  const { isLoading } = useGlobalContext();
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+  return (
+    <main>
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/context.js
@@ -0,0 +1,72 @@
+import React, { useContext, useReducer, useEffect } from "react";
+import cartItems from "./data";
+import reducer from "./reducer";
+
+const url = "https://course-api.com/react-useReducer-cart-project";
+const AppContext = React.createContext();
+
+const initialState = {
+  isLoading: false,
+  cart: cartItems,
+  amount: 0,
+  total: 0,
+};
+
+const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const clearCart = () => {
+    dispatch({ type: "CLEAR_CART" });
+  };
+
+  const removeItem = (id) => {
+    dispatch({ type: "REMOVE_ITEM", payload: id });
+  };
+
+  const increase = (id) => {
+    dispatch({ type: "INCREASE", payload: id });
+  };
+
+  const decrease = (id) => {
+    dispatch({ type: "DECREASE", payload: id });
+  };
+
+  // const toggleAmount = (id, type) => {
+  //   dispatch({ type: "TOGGLE_AMOUNT", payload: { id, type } });
+  // };
+
+  const fetchData = async () => {
+    dispatch({ type: "LOADING" });
+    const res = await fetch(url);
+    const cart = await res.json();
+    dispatch({ type: "DISPLAY_ITEMS", payload: cart });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    dispatch({ type: "GET_TOTALS" });
+  }, [state.cart]);
+
+  return (
+    <AppContext.Provider
+      value={{
+        ...state,
+        clearCart,
+        removeItem,
+        increase,
+        decrease,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+// make sure use
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
